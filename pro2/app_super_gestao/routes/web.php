9deda6a6e54f40eba9739e35466529149d5517b8
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-04-23 11:28:11 -0300

    Iniciando a criação de views

--- pro2/app_super_gestao/routes/web.php
+++ pro2/app_super_gestao/routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', "PrincipalController@principal");
+Route::get('/sobrenos', "SobreNosController@sobrenos");
+Route::get('/contato', "ContatoController@contato");
