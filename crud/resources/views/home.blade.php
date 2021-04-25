fdc81089e2255c9c92de06798181c683b9e424e5
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-04-25 20:16:54 -0300

    migration tabela noticias foi criada e area para postar noticias já add

--- crud/resources/views/home.blade.php
+++ crud/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Notícias') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,8 +15,21 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    
                 </div>
             </div>
+            <form style="margin-top: 20px">
+                <div class="form-group">
+                  <label for="exampleFormControlInput1">Título</label>
+                  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Informativo..." maxlength="200">
+                </div>
+                
+                <div class="form-group">
+                  <label for="exampleFormControlTextarea1">Exemplo de textarea</label>
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+              </form>
+              <button type="button" class="btn btn-primary btn-lg btn-block">Postar</button>
         </div>
     </div>
 </div>
